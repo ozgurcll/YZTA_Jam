1b1cd8c88288124b5150d600caa3ab7e2ce91d89
Özgür <ozgur.calli52@gmail.com>
2025-05-03 23:54:33 +0300

    .

--- Assets/Script/Enemy/EnemyStateMachine.cs
+++ Assets/Script/Enemy/EnemyStateMachine.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 
-public class EnemyStateMachine : MonoBehaviour
+public class EnemyStateMachine
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public EnemyState currentState { get; private set; }
+
+    public void Initialize(EnemyState _startState)
     {
-        
+        currentState = _startState;
+        currentState.Enter();
+    }
+    public void ChangeState(EnemyState _newState)
+    {
+        currentState.Exit();
+        currentState = _newState;
+        currentState.Enter();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
